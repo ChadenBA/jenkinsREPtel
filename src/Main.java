559da6e45f643fae61bc99ea4e829da9338bce74
chaden ben ammar <59190965+ChadenBA@users.noreply.github.com>
2024-01-26 19:12:22 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,26 @@
-import persistance.PersonneDAO;
-import persistance.PersonneDAOImpl;
-import persistance.TelDAOImpl;
-import metier.Personne;
-import metier.Telephone;
+//import persistance.PersonneDAO;
+//import persistance.PersonneDAOImpl;
+//import persistance.TelDAOImpl;
+//import metier.Personne;
+//import metier.Telephone;
 
 public class Main {
     public static void main(String[] args) {
         try {
             // Create an instance of your TelDAO implementation
-            TelDAOImpl telDAOImpl = new TelDAOImpl();
+          //  TelDAOImpl telDAOImpl = new TelDAOImpl();
 
           
 
             // Create a sample Telephone object for update
-            Telephone telephoneToUpdate = new Telephone();
-            telephoneToUpdate.setCin(1555); // Replace with the actual cin
-            telephoneToUpdate.setType("T"); // Replace with the actual type
-            telephoneToUpdate.setValeur(7887); // Replace with the actual valeur
+            //Telephone telephoneToUpdate = new Telephone();
+            //telephoneToUpdate.setCin(1555); // Replace with the actual cin
+            //telephoneToUpdate.setType("T"); // Replace with the actual type
+            //telephoneToUpdate.setValeur(7887); // Replace with the actual valeur
 
             // Update the Telephone record
-            telDAOImpl.modifiertel(telephoneToUpdate);
-
+            //telDAOImpl.modifiertel(telephoneToUpdate);
+ System.out.println("my tel rep app ");
         } catch (Exception e) {
             e.printStackTrace();
         }
